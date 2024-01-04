634fcbc1880a6e3f873eeecab7d6fd190efefff9
CatoTheYounger97 <82458228+CatoTheYounger97@users.noreply.github.com>
2024-01-04 21:31:33 +0000

    Create template files and dependancies

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import "./style.css";
+
+const templateText = document.createElement("div");
+templateText.classList.add("TemplateText");
+templateText.textContent = "TEMPLATE";
+
+document.body.appendChild(templateText);
